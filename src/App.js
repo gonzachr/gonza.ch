31a8a8a4b1ed87a457eac409c5af7c275c018e71
gonza <gonzalo.christobal@gmail.com>
2019-08-03 18:23:11 -0400

    First three boxes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from "react";
+import { Canvas } from "react-three-fiber";
+import { useSpring } from "react-spring/three";
+import Box from "./Box";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [{ mouse }, set] = useSpring(() => ({ mouse: [0, 0] }));
+  const onMouseMove = useCallback(
+    ({ clientX: x, clientY: y }) => {
+      set({ mouse: [x - window.innerWidth / 2, y - window.innerHeight / 2] });
+    },
+    [set]
   );
-}
+
+  return (
+    <Canvas className="canvas" onMouseMove={onMouseMove}>
+      <directionalLight color={0xffffff} intensity={1} position={[-1, 2, 4]} />
+      <spotLight color={0xffffff} position={[10, 5, 0]} />
+      <Box mouse={mouse} position={{ y: 0, x: -3 }} color={0x161429} />
+      <Box mouse={mouse} position={{ y: 0, x: 0 }} color={0xcd979c} />
+      <Box mouse={mouse} position={{ y: 0, x: 3 }} color={0xddbabd} />
+    </Canvas>
+  );
+};
 
 export default App;
