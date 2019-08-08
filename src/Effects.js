ffcbeadd0475aed667a95b961ac861c8b8271145
gonza <gonzalo.christobal@gmail.com>
2019-08-08 13:53:11 -0400

    Create Effects

---
+++ src/Effects.js
@@ -0,0 +1,29 @@
+import React, { useRef, useEffect } from "react";
+import { useThree, useRender } from "react-three-fiber";
+import { a } from "react-spring/three";
+
+const Effects = React.memo(({ factor }) => {
+  const pixelRatio = window.devicePixelRatio;
+  const { gl, scene, camera, size } = useThree();
+  const composer = useRef();
+  
+  useEffect(() => {
+    return void composer.current.setSize(
+      size.width * pixelRatio,
+      size.height * pixelRatio
+    );
+  }, [size, pixelRatio]);
+  useRender(() => {
+    return composer.current.render();
+  }, true);
+
+  return (
+    <effectComposer ref={composer} args={[gl]}>
+      <renderPass attachArray="passes" args={[scene, camera]} />
+      <filmPass attachArray="passes" args={[0.5, 0.025, 648, false]} />
+      <a.glitchPass attachArray="passes" renderToScreen factor={factor} />
+    </effectComposer>
+  );
+});
+
+export default Effects;
