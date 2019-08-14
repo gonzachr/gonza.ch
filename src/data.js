13ddbd0f1e52d8f394f478182b28fd99b6352786
gonza <gonzalo.christobal@gmail.com>
2019-08-14 12:44:41 -0400

    Data file created

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    key: "Intro",
+    text: { __html: "Hola!" },
+    css: { fontSize: "2em", fontWeight: 600, marginBottom: "1em" }
+  },
+  {
+    key: "Name",
+    text: { __html: "I'm Gonzalo Rodríguez Torres" },
+    css: { fontSize: "2em", fontWeight: 600, marginBottom: "1em" }
+  },
+  {
+    key: "Content",
+    text: { __html: "Sup bro" },
+    // text: {
+    //   __html: `I'm a self-taught developer from Paraguay (South America), i started programming 4 years ago and since i never stop 
+    // <a
+    //   target="blank"
+    //   href="https://github.com/FundacionParaguaya/stoplight-web"
+    // >
+    //   complex applications
+    // </a>
+    // , with a sense on
+    // <a
+    //   target="blank"
+    //   href="https://github.com/FundacionParaguaya/stoplight-web/pull/406"
+    // >
+    //   engineering intuition
+    // </a>
+    // , and somewhat good
+    // <a
+    //   target="blank"
+    //   href="https://github.com/FundacionParaguaya/stoplight-web/pull/430"
+    // >
+    //   communication skills
+    // </a>`
+    // },
+    css: {
+      fontSize: "1em",
+      fontWeight: 300,
+      lineHeight: 2,
+      marginBottom: "2em"
+    }
+  },
+  {
+    key: "Interests",
+    text: { __html: "Current interests" },
+    css: { fontSize: "1.2em", fontWeight: 600, marginBottom: "1.5em" }
+  },
+  {
+    key: "Interests Content",
+    text: {
+      __html: `Fucking black clouds`
+    },
+    css: {
+      fontSize: "1em",
+      fontWeight: 300,
+      lineHeight: 2,
+      marginBottom: "2em"
+    }
+  }
+];
