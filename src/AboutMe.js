a635247bd067608c77cf5abdd6fa9f6cd08fb609
gonza <gonzalo.christobal@gmail.com>
2019-08-14 12:45:06 -0400

    Add transition

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,8 +1,10 @@
-import React from "react";
-import { useSpring, animated } from "react-spring";
+import React, { useRef, useEffect, useState } from "react";
+import { animated, useTransition, useChain } from "react-spring";
 import { VIEW_ABOUT_ME } from "./redux/actions";
 import { connect } from "react-redux";
 import styled from "styled-components";
+import { setTransRef } from "./redux/actions";
+import items from "./data";
 
 const FrontLine = styled.div`
   font-weight: 600;
@@ -10,18 +12,90 @@ const FrontLine = styled.div`
   margin-bottom: 2rem;
 `;
 
-const AboutMe = ({ currentView }) => {
-  const { opacity } = useSpring({
-    opacity: currentView === VIEW_ABOUT_ME ? 1 : 0
+const AboutMe = ({ currentView, currentRef, setTransRef }) => {
+  const transRef = useRef();
+  const transitions = useTransition(
+    currentView === VIEW_ABOUT_ME ? items : [],
+    item => item.key,
+    {
+      ref: transRef,
+      unique: true,
+      trail: 400 / items.length,
+      from: { opacity: 0, transform: "translateY(50%)" },
+      enter: { opacity: 1, transform: "translateY(0%)" },
+      leave: { opacity: 0, transform: "translateY(50%)" }
+    }
+  );
+  // const refTemp = useRef();
+
+  useEffect(() => {
+    if (currentRef !== transRef) {
+      setTransRef(transRef);
+    }
   });
 
+  // useChain(
+  //   currentView === VIEW_ABOUT_ME
+  //     ? [currentRef.springRef ? currentRef.springRef : refTemp, transRef]
+  //     : [
+  //         (transRef: refTemp),
+  //         currentRef.springRef ? currentRef.springRef : refTemp
+  //       ],
+  //   [0, currentView === VIEW_ABOUT_ME ? 0.5 : 1]
+  // );
+
   return (
-    <animated.div className="about-me" style={{ opacity }}>
-      <FrontLine>In Progress..</FrontLine>
-    </animated.div>
+    <div className="about-me">
+      {transitions.map(({ item, key, props }) => (
+        <animated.p
+          key={key}
+          style={{ ...props, ...item.css }}
+          dangerouslySetInnerHTML={item.text}
+        >
+          {/* {item.text} */}
+          {/* <FrontLine>
+            Hola! I'm Gonzalo Rodríguez, a 20 years old self-taught Frontend
+            Developer.
+          </FrontLine>
+          <div>
+            Since my first game in Visual Basic (don't judge me) i never stop
+            using and learning from my computer, i may found a passion. Regards
+            my professional career, i have experience building{" "}
+            <a
+              target="blank"
+              href="https://github.com/FundacionParaguaya/stoplight-web"
+            >
+              complex applications
+            </a>{" "}
+            , with a sense on{" "}
+            <a
+              target="blank"
+              href="https://github.com/FundacionParaguaya/stoplight-web/pull/406"
+            >
+              engineering intuition
+            </a>
+            , and somewhat good{" "}
+            <a
+              target="blank"
+              href="https://github.com/FundacionParaguaya/stoplight-web/pull/430"
+            >
+              communication skills
+            </a>
+            .
+          </div> */}
+        </animated.p>
+      ))}
+    </div>
   );
 };
 
-const mapStateToProps = currentView => ({ currentView });
+const mapStateToProps = ({ currentView, currentRef }) => ({
+  currentView,
+  currentRef
+});
+const mapDispatchToProps = { setTransRef };
 
-export default connect(mapStateToProps)(AboutMe);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AboutMe);
--- src/Model.js
+++ src/Model.js
@@ -1,32 +1,49 @@
 import React, { useEffect, useRef, useState } from "react";
 import { unstable_createResource as createResource } from "./react-cache";
-import { a, useSpring } from "react-spring/three";
+import { a, useSpring, config, useChain } from "react-spring/three";
 import { Math as ThreeMath } from "three";
 import { useRender } from "react-three-fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { Views } from "./redux/actions";
+import { Views, toggleLoading } from "./redux/actions";
+import delay from "delay";
+import store from "./redux";
 
 const path = "./models/head/";
 
 const resource = createResource(
-  file => new Promise(async res => new GLTFLoader().load(path + file, res))
+  file =>
+    new Promise(async res => {
+      return (
+        await delay(2500),
+        store.dispatch(toggleLoading()),
+        new GLTFLoader().load(path + file, res)
+      );
+    })
 );
 
-function Model({ mouse, position, top, currentView, ...other }) {
-  const [currentRotation, setCurrentRotation] = useState([0, 0, 0]);
+function Model({
+  mouse,
+  top,
+  currentView,
+  currentRef,
+  setSpringRef,
+  ...other
+}) {
+  const [currentRotation, setCurrentRotation] = useState([0, -3.2, 0]);
   const [positionX, setPositionX] = useState(0);
-  const [rotationX, setRotationX] = useState(-2.5);
-  const [piTimes, setPiTimes] = useState(0);
-  const x = position[0];
   const file = "scene.gltf";
-  const timesX = ThreeMath.mapLinear(x, 0, 6, 0, window.innerWidth / 2);
+  const tempRef = useRef();
+  // const [piTimes, setPiTimes] = useState(0);
+  // const x = position[0];
+  // const [rotationX, setRotationX] = useState(-2.5);
+  // const primitive = useRef();
+  // const timesX = ThreeMath.mapLinear(x, 0, 6, 0, window.innerWidth / 2);
+  // let s = 0;
   const width = window.innerWidth / 2;
   const height = window.innerHeight / 2;
   const { scene } = resource.read(file);
-  const primitive = useRef();
   let theta = 1;
-  let s = 0;
-  const { VIEW_ABOUT_ME, VIEW_CONTACT } = Views;
+  const { VIEW_ABOUT_ME } = Views;
 
   // const mouseMove = mouse.interpolate((x, y) => {
   //   const rotX = ThreeMath.mapLinear(x, 0, width, -3, -2);
@@ -35,25 +52,45 @@ function Model({ mouse, position, top, currentView, ...other }) {
   //   return [rotY, rotX, 0];
   // });
 
-  useEffect(() => {
-    const [x, y] = mouse;
-    const rotX = ThreeMath.mapLinear(x, 0, width, -3, -2);
-    const rotY = ThreeMath.mapLinear(y, 0, height, 0, 0.5);
-
-    setCurrentRotation([rotY, rotX, 0]);
-  }, [mouse, width, height]);
-
-  const { pos, rotation } = useSpring({
-    pos: currentView === VIEW_ABOUT_ME ? [-2, positionX, 2] : [0, positionX, 0],
-    rotation:
-      currentView === VIEW_ABOUT_ME ? [0, rotationX, 0] : currentRotation
+  const springRef = useRef();
+  const { rotation, position, opacity, ...rest } = useSpring({
+    ref: springRef,
+    config: config.gentle,
+    from: {
+      opacity: 0,
+      position: [0, positionX, 0],
+      rotation: currentRotation
+    },
+    to: {
+      opacity: 1,
+      position:
+        currentView === VIEW_ABOUT_ME ? [-2, positionX, 2] : [0, positionX, 0],
+      rotation: currentView === VIEW_ABOUT_ME ? [0, -2.5, 0] : currentRotation
+    }
+    // pos: currentView === VIEW_ABOUT_ME ? [-2, positionX, 2] : [0, positionX, 0],
+    // rotation:
+    //   currentView === VIEW_ABOUT_ME ? [0, rotationX, 0] : currentRotation
   });
 
-  useRender(() => {
-    s += 1;
-    let rounded = piTimes !== 0 ? s / (piTimes + 1) - 360 : s / (piTimes + 1);
-    let currentRotation = ThreeMath.degToRad(rounded);
+  useEffect(() => {
+    const [x, y] = mouse;
+    const rotX = ThreeMath.mapLinear(x, 0, width, -3.15, -2);
+    const rotY = ThreeMath.mapLinear(y, 0, height, 0, 0.5);
 
+    setSpringRef(springRef);
+    setCurrentRotation([rotY, rotX, 0]);
+  }, [mouse, width, height, currentView, setSpringRef]);
+  const ref = currentRef.transRef ? currentRef.transRef : tempRef;
+
+  useChain(
+    currentView === VIEW_ABOUT_ME ? [springRef, ref] : [ref, springRef],
+    [0, currentView === VIEW_ABOUT_ME ? 0.5 : 1]
+  );
+
+  useRender(() => {
+    // s += 1;
+    // let rounded = piTimes !== 0 ? s / (piTimes + 1) - 360 : s / (piTimes + 1);
+    // let currentRotation = ThreeMath.degToRad(rounded);
     const r = ThreeMath.mapLinear(
       Math.sin(ThreeMath.degToRad((theta += 2))),
       -1,
@@ -70,11 +107,14 @@ function Model({ mouse, position, top, currentView, ...other }) {
   return (
     <a.primitive
       {...other}
-      ref={primitive}
+      {...rest}
       object={scene}
       scale={[0.8, 0.8, 0.8]}
       rotation={rotation}
-      position={pos}
+      position={position}
+      transparent
+      opacity={opacity}
+      // ref={primitive}
       // position={top.interpolate(top => {
       //   const y = ThreeMath.mapLinear(top, 0, window.innerHeight, 0, -2);
       //   const z = ThreeMath.mapLinear(top, 0, window.innerHeight, 0, 2);
@@ -85,4 +125,5 @@ function Model({ mouse, position, top, currentView, ...other }) {
     />
   );
 }
+
 export default Model;
