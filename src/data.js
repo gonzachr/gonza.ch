d3ec3826f1fdce06c33729744e770766a5232ea5
gonza <gonzalo.christobal@gmail.com>
2019-08-14 15:22:34 -0400

    Update data

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export default [
   {
     key: "Intro",
-    text: { __html: "Hola!" },
+    text: { __html: "Hola! 👋" },
     css: { fontSize: "2em", fontWeight: 600, marginBottom: "1em" }
   },
   {
@@ -11,46 +11,57 @@ export default [
   },
   {
     key: "Content",
-    text: { __html: "Sup bro" },
-    // text: {
-    //   __html: `I'm a self-taught developer from Paraguay (South America), i started programming 4 years ago and since i never stop 
-    // <a
-    //   target="blank"
-    //   href="https://github.com/FundacionParaguaya/stoplight-web"
-    // >
-    //   complex applications
-    // </a>
-    // , with a sense on
-    // <a
-    //   target="blank"
-    //   href="https://github.com/FundacionParaguaya/stoplight-web/pull/406"
-    // >
-    //   engineering intuition
-    // </a>
-    // , and somewhat good
-    // <a
-    //   target="blank"
-    //   href="https://github.com/FundacionParaguaya/stoplight-web/pull/430"
-    // >
-    //   communication skills
-    // </a>`
-    // },
-    css: {
-      fontSize: "1em",
-      fontWeight: 300,
-      lineHeight: 2,
-      marginBottom: "2em"
-    }
-  },
-  {
-    key: "Interests",
-    text: { __html: "Current interests" },
-    css: { fontSize: "1.2em", fontWeight: 600, marginBottom: "1.5em" }
-  },
-  {
-    key: "Interests Content",
     text: {
-      __html: `Fucking black clouds`
+      __html: `I'm a Frontend Developer from Paraguay (South America), after many years writing code, i consider myself a skilled JavaScript developer with a deep understanding of the latest internet technologies, a keen 
+    <a
+      target="blank"
+      
+      href="https://github.com/FundacionParaguaya/stoplight-web/pull/406"
+    >
+      engineering intuition
+    </a>
+    , and outstanding 
+    <a
+      target="blank"
+      href="https://github.com/FundacionParaguaya/stoplight-web/pull/430"
+      
+    >
+    communication skills.
+    </a> Aditionally i have experience building
+    <a
+      target="blank"
+      href="https://github.com/FundacionParaguaya/stoplight-web/graphs/contributors"
+    >
+      complex applications
+    </a>, solid knowledge of modern 
+    <a
+      target="blank"
+      href="https://github.com/drcmda/react-three-fiber/pull/157"
+    >
+    JavaScript and React 
+    </a>
+    and 
+    <a
+      target="blank"
+      href="https://github.com/gonzarodriguezt/todoist-clone/tree/master/src/redux"
+    >
+    Redux
+    </a>
+    as a Bonus, also some experience 
+    <a
+      target="blank"
+      href="https://github.com/gonzarodriguezt/todoist-clone/tree/master/src/__tests__"
+    >
+    writing tests
+    </a>
+    . Along with ability to use and 
+    <a
+      target="blank"
+      href="https://github.com/gonzarodriguezt/create-dev-server"
+    >
+    customize tools 
+    </a>
+    to streamline development and build processes.`
     },
     css: {
       fontSize: "1em",
@@ -58,5 +69,22 @@ export default [
       lineHeight: 2,
       marginBottom: "2em"
     }
-  }
+  },
+  // {
+  //   key: "Interests",
+  //   text: { __html: "Current interests" },
+  //   css: { fontSize: "1.2em", fontWeight: 600, marginBottom: "1.5em" }
+  // },
+  // {
+  //   key: "Current Interests",
+  //   text: {
+  //     __html: `I'm now currently learning`
+  //   },
+  //   css: {
+  //     fontSize: "1em",
+  //     fontWeight: 300,
+  //     lineHeight: 2,
+  //     marginBottom: "2em"
+  //   }
+  // }
 ];
