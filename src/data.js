1a8d0688399037e331226e4debf2cfe3be075302
gonza <gonzalo.christobal@gmail.com>
2019-10-11 12:54:50 -0300

    daw

--- src/data.js
+++ src/data.js
@@ -1,32 +1,18 @@
 export default [
   {
     key: "Intro",
-    text: { __html: "Hola! 👋" },
-    css: { fontSize: "2em", fontWeight: 600, marginBottom: "1em" }
+    text: { __html: "hey" },
+    css: { fontSize: "1em", fontWeight: 600, marginBottom: "1em" }
   },
   {
     key: "Name",
-    text: { __html: "I'm Gonzalo Rodríguez Torres" },
-    css: { fontSize: "2em", fontWeight: 600, marginBottom: "1em" }
+    text: { __html: "im just a guy" },
+    css: { fontSize: "1em", fontWeight: 600, marginBottom: "1em" }
   },
   {
     key: "Content",
     text: {
-      __html: `I'm a skilled JavaScript developer with a deep understanding of the latest internet technologies, a keen 
-    <a
-      target="blank"
-      
-      href="https://github.com/FundacionParaguaya/stoplight-web/pull/406"
-    >
-      engineering intuition
-    </a>, and outstanding 
-    <a
-      target="blank"
-      href="https://github.com/FundacionParaguaya/stoplight-web/pull/430"
-    >
-    communication skills.
-    </a>
-    (See edits).`
+      __html: `trying to live a good life, fighting with some internal stuff along the way, and i'm not talking about call stacks nor rams, hope ur doing okay, love u, stranger`
     },
     css: {
       fontSize: "1em",
@@ -35,113 +21,4 @@ export default [
       marginBottom: "1em"
     }
   },
-  {
-    key: "Aditionally",
-    text: {
-      __html: `Aditionally, i have experience building
-    <a
-      target="blank"
-      href="https://github.com/FundacionParaguaya/stoplight-web/graphs/contributors"
-    >
-      complex applications
-    </a>, solid knowledge of modern 
-    <a
-      target="blank"
-      href="https://github.com/drcmda/react-three-fiber/pull/157"
-    >
-    JavaScript and React
-    </a>
-     and 
-    <a
-      target="blank"
-      href="https://github.com/gonzarodriguezt/gonza.ch/blob/master/src/redux/reducers.js"
-    >
-    Redux
-    </a>
-     just as a bonus. I also came to have some experience 
-    <a
-      target="blank"
-      href="https://github.com/gonzarodriguezt/todoist-clone/tree/master/src/__tests__"
-    >
-    writing tests
-    </a>
-    . Along with ability to use and 
-    <a
-      target="blank"
-      href="https://github.com/gonzarodriguezt/create-dev-server"
-    >
-    customize tools 
-    </a>
-    to streamline development and build processes.`
-    },
-    css: {
-      fontSize: "1em",
-      fontWeight: 300,
-      lineHeight: 2,
-      marginBottom: "1em"
-    }
-  },
-  {
-    key: "Coincidentally",
-    text: {
-      __html: `I coincidentally count with experience working in a 
-      <a
-      target="blank"
-      href="https://github.com/FundacionParaguaya"
-      >
-      remote team
-      </a> 
-      and fluency writing and speaking in English. With no intention to leave behind my 
-      <a
-        target="blank"
-        href="https://github.com/gonzarodriguezt/gonza.ch/graphs/contributors"
-      >
-      proven ability
-      </a> 
-      to work autonomously and stay self-motivated.`
-    },
-    css: {
-      fontSize: "1em",
-      fontWeight: 300,
-      lineHeight: 2,
-      marginBottom: "2em"
-    }
-  },
-  {
-    key: "Projects",
-    text: { __html: "Some Projects" },
-    css: { fontSize: "1.2em", fontWeight: 600, marginBottom: "1.5em" }
-  },
-  {
-    key: "Current Interests",
-    text: {
-      __html: `✅ <a
-      target="blank"
-      href="https://todoist.gonza.ch/"
-    >
-    Todoist Clone
-    <br />
-    </a>
-    ⚔️ <a
-      target="blank"
-      href="https://battle.gonza.ch"
-    >
-    GitHub Battle
-    </a>
-    <br /> 
-    🇵🇾 <a
-    target="blank"
-    href="http://assistan-ces.surge.sh/Q2F0ZXVyYQ==/MjktMDQtMjAxOQ=="
-    >
-    Assistance App
-    </a>
-    `
-    },
-    css: {
-      fontSize: "1em",
-      fontWeight: 300,
-      lineHeight: 2,
-      marginBottom: "2em"
-    }
-  }
 ];
